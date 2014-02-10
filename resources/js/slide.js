d3ca59022e69d0a474b9641174b6e42c1459dcc4
gefangshuai <gefangshuai@163.com>
2014-02-10 22:11:12 +0800

    加入说明文档

---
+++ resources/js/slide.js
@@ -0,0 +1,792 @@
+// media - 1
+var mediaSlideData1 = {
+  medias: [
+    {
+      pic: '3.jpg', 
+      url: 'index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../2/3.jpg', 
+      url: '../2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../4/2.jpg', 
+      url: '../4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../5/1.jpg', 
+      url: '../5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData1 = {
+  medias: [
+    {
+      pic: '../../pieces/1/1.jpg', 
+      url: '../../pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../../pieces/2/1.jpg', 
+      url: '../../pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../../pieces/3/1.jpg', 
+      url: '../../pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../../pieces/4/1.jpg', 
+      url: '../../pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../../pieces/5/2.jpg', 
+      url: '../../pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../../pieces/6/1.jpg', 
+      url: '../../pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 2
+var mediaSlideData2 = {
+  medias: [
+    {
+      pic: '../1/3.jpg', 
+      url: '../1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '3.jpg', 
+      url: 'index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../4/2.jpg', 
+      url: '../4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../5/1.jpg', 
+      url: '../5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData2 = {
+  medias: [
+    {
+      pic: '../../pieces/1/1.jpg', 
+      url: '../../pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../../pieces/2/1.jpg', 
+      url: '../../pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../../pieces/3/1.jpg', 
+      url: '../../pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../../pieces/4/1.jpg', 
+      url: '../../pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../../pieces/5/2.jpg', 
+      url: '../../pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../../pieces/6/1.jpg', 
+      url: '../../pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 3
+var mediaSlideData3 = {
+  medias: [
+    {
+      pic: '../1/3.jpg', 
+      url: '../1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../2/3.jpg', 
+      url: '../2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../4/2.jpg', 
+      url: '../4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../5/1.jpg', 
+      url: '../5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData3 = {
+  medias: [
+    {
+      pic: '../../pieces/1/1.jpg', 
+      url: '../../pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../../pieces/2/1.jpg', 
+      url: '../../pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../../pieces/3/1.jpg', 
+      url: '../../pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../../pieces/4/1.jpg', 
+      url: '../../pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../../pieces/5/2.jpg', 
+      url: '../../pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../../pieces/6/1.jpg', 
+      url: '../../pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 4
+var mediaSlideData4 = {
+  medias: [
+    {
+      pic: '../1/3.jpg', 
+      url: '../1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../2/3.jpg', 
+      url: '../2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '2.jpg', 
+      url: 'index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../5/1.jpg', 
+      url: '../5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData4 = {
+  medias: [
+    {
+      pic: '../../pieces/1/1.jpg', 
+      url: '../../pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../../pieces/2/1.jpg', 
+      url: '../../pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../../pieces/3/1.jpg', 
+      url: '../../pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../../pieces/4/1.jpg', 
+      url: '../../pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../../pieces/5/2.jpg', 
+      url: '../../pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../../pieces/6/1.jpg', 
+      url: '../../pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 5
+var mediaSlideData5= {
+  medias: [
+    {
+      pic: '../1/3.jpg', 
+      url: '../1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../2/3.jpg', 
+      url: '../2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../4/2.jpg', 
+      url: '../4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData5 = {
+  medias: [
+    {
+      pic: '../../pieces/1/1.jpg', 
+      url: '../../pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../../pieces/2/1.jpg', 
+      url: '../../pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../../pieces/3/1.jpg', 
+      url: '../../pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../../pieces/4/1.jpg', 
+      url: '../../pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../../pieces/5/2.jpg', 
+      url: '../../pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../../pieces/6/1.jpg', 
+      url: '../../pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 6
+var mediaSlideData6 = {
+  medias: [
+    {
+      pic: '../1/3.jpg', 
+      url: '../1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../2/3.jpg', 
+      url: '../2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../4/2.jpg', 
+      url: '../4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../5/1.jpg', 
+      url: '../5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData6= {
+  medias: [
+    {
+      pic: '../../pieces/1/1.jpg', 
+      url: '../../pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../../pieces/2/1.jpg', 
+      url: '../../pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../../pieces/3/1.jpg', 
+      url: '../../pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../../pieces/4/1.jpg', 
+      url: '../../pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../../pieces/5/2.jpg', 
+      url: '../../pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../../pieces/6/1.jpg', 
+      url: '../../pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// pieces - 1
+var mediaSlideData11 = {
+  medias: [
+    {
+      pic: '../../media/1/3.jpg', 
+      url: '../../media/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../../media/2/3.jpg', 
+      url: '../../media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../../media/3/1.jpg', 
+      url: '../../media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../../media/4/2.jpg', 
+      url: '../../media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../../media/5/1.jpg', 
+      url: '../../media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../../media/6/1.jpg', 
+      url: '../../media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData11 = {
+  medias: [
+    {
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../2/1.jpg', 
+      url: '../2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../4/1.jpg', 
+      url: '../4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../5/2.jpg', 
+      url: '../5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 2
+var mediaSlideData22 = {
+  medias: [
+    {
+      pic: '../../media/1/3.jpg', 
+      url: '../../media/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../../media/2/3.jpg', 
+      url: '../../media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../../media/3/1.jpg', 
+      url: '../../media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../../media/4/2.jpg', 
+      url: '../../media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../../media/5/1.jpg', 
+      url: '../../media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../../media/6/1.jpg', 
+      url: '../../media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData22 = {
+  medias: [
+    {
+      pic: '../1/1.jpg', 
+      url: '../1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../4/1.jpg', 
+      url: '../4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../5/2.jpg', 
+      url: '../5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 3
+var mediaSlideData33 = {
+  medias: [
+    {
+      pic: '../../media/1/3.jpg', 
+      url: '../../media/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../../media/2/3.jpg', 
+      url: '../../media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../../media/3/1.jpg', 
+      url: '../../media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../../media/4/2.jpg', 
+      url: '../../media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../../media/5/1.jpg', 
+      url: '../../media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../../media/6/1.jpg', 
+      url: '../../media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData33 = {
+  medias: [
+    {
+      pic: '../1/1.jpg', 
+      url: '../1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../2/1.jpg', 
+      url: '../2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../4/1.jpg', 
+      url: '../4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../5/2.jpg', 
+      url: '../5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 4
+var mediaSlideData44 = {
+  medias: [
+    {
+      pic: '../../media/1/3.jpg', 
+      url: '../../media/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../../media/2/3.jpg', 
+      url: '../../media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../../media/3/1.jpg', 
+      url: '../../media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../../media/4/2.jpg', 
+      url: '../../media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../../media/5/1.jpg', 
+      url: '../../media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../../media/6/1.jpg', 
+      url: '../../media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData44 = {
+  medias: [
+    {
+      pic: '../1/1.jpg', 
+      url: '../1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../2/1.jpg', 
+      url: '../2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../5/2.jpg', 
+      url: '../5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 5
+var mediaSlideData55 = {
+  medias: [
+    {
+      pic: '../../media/1/3.jpg', 
+      url: '../../media/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../../media/2/3.jpg', 
+      url: '../../media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../../media/3/1.jpg', 
+      url: '../../media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../../media/4/2.jpg', 
+      url: '../../media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../../media/5/1.jpg', 
+      url: '../../media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../../media/6/1.jpg', 
+      url: '../../media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData55 = {
+  medias: [
+    {
+      pic: '../1/1.jpg', 
+      url: '../1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../2/1.jpg', 
+      url: '../2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../4/1.jpg', 
+      url: '../4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '2.jpg', 
+      url: 'index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '../6/1.jpg', 
+      url: '../6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
+// media - 6
+var mediaSlideData66 = {
+  medias: [
+    {
+      pic: '../../media/1/3.jpg', 
+      url: '../../media/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: '../../media/2/3.jpg', 
+      url: '../../media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: '../../media/3/1.jpg', 
+      url: '../../media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: '../../media/4/2.jpg', 
+      url: '../../media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: '../../media/5/1.jpg', 
+      url: '../../media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: '../../media/6/1.jpg', 
+      url: '../../media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
+    }
+  ]
+}
+
+var piecesSlideData66 = {
+  medias: [
+    {
+      pic: '../1/1.jpg', 
+      url: '../1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
+    },
+    {
+      pic: '../2/1.jpg', 
+      url: '../2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: '../3/1.jpg', 
+      url: '../3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: '../4/1.jpg', 
+      url: '../4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: '../5/2.jpg', 
+      url: '../5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: '1.jpg', 
+      url: 'index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
+    }
+  ]
+}
+
