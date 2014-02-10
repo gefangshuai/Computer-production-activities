eeea4f33a329ce6e7cf1ea1d63c1a223a88049f6
gefangshuai <gefangshuai@163.com>
2014-02-10 21:40:33 +0800

    finish

--- resources/js/display.js
+++ resources/js/display.js
@@ -4,21 +4,61 @@ var mediaDisplayData = {
       pic: 'media/1/3.jpg', 
       url: 'media/1/index.html', 
       title: '长大后给爸爸妈妈幸福的家'
+    },{
+      pic: 'media/2/3.jpg', 
+      url: 'media/2/index.html', 
+      title: '屯堡中学：让爱心永存人间！'
+    },{
+      pic: 'media/3/1.jpg', 
+      url: 'media/3/index.html', 
+      title: '捐助者：希望她做一个对社会有用的人'
+    },{
+      pic: 'media/4/2.jpg', 
+      url: 'media/4/index.html', 
+      title: '甘塘小学：用心作图，规划人生'
+    },{
+      pic: 'media/5/1.jpg', 
+      url: 'media/5/index.html', 
+      title: '捐助者：捐助永远没有句号'
+    },{
+      pic: 'media/6/1.jpg', 
+      url: 'media/6/index.html', 
+      title: '某县民族中学：让他们有机会好好读书'
     }
   ]
 }
 
-var piecesDisplaysDisplayData = {
+var piecesDisplaysData = {
   pics: [
     {
-      pic: 1, 
-      url: 'media/1/index.html', 
-      title: '长大后给爸爸妈妈幸福的家'
+      pic: 'pieces/1/1.jpg', 
+      url: 'pieces/1/index.html', 
+      title: '那些重庆山区里可爱的孩子们！'
     },
     {
-      pic: 2,
-      url: 'http://www.owecn.com/Pieces/20090316/200903164649.html',
-      title: '我梦想着飞翔到更远的地方'
+      pic: 'pieces/2/1.jpg', 
+      url: 'pieces/2/index.html', 
+      title: '其实生命对每一个人都是公平的'
+    },
+    {
+      pic: 'pieces/3/1.jpg', 
+      url: 'pieces/3/index.html', 
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: 'pieces/4/1.jpg', 
+      url: 'pieces/4/index.html', 
+      title: '美丽中国：与长城眺眺相望的白衣庵'
+    },
+    {
+      pic: 'pieces/5/2.jpg', 
+      url: 'pieces/5/index.html', 
+      title: '生命就应该得到敬畏和尊重！'
+    },
+    {
+      pic: 'pieces/6/1.jpg', 
+      url: 'pieces/6/index.html', 
+      title: '美丽中国：你见过如此美的晚霞吗'
     }
   ]
 }
