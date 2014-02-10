03f0ec204ff32150eed7420225162737bfc2c31a
gefangshuai <gefangshuai@163.com>
2014-02-10 16:37:55 +0800

    完成文章浏览页面的设计

---
+++ resources/js/display.js
@@ -0,0 +1,49 @@
+var displayData = {
+  pics: [
+    {
+      pic: 1, 
+      url: 'display/1/index.html', 
+      title: '长大后给爸爸妈妈幸福的家'
+    },
+    {
+      pic: 2,
+      url: 'http://www.owecn.com/Pieces/20090316/200903164649.html',
+      title: '我梦想着飞翔到更远的地方'
+    },
+    {
+      pic: 3,
+      url: 'http://www.owecn.com/Pieces/20070616/20070616806.html',
+      title: '路连向远方，改变了我们的生活！'
+    },
+    {
+      pic: 4,
+      url: 'http://www.owecn.com/Life/20140109/2014010926797.html',
+      title: '尤达村校：爱心是黎明前的一道曙光！'
+    },
+    {
+      pic: 5,
+      url: 'http://www.owecn.com/Life/20121118/2012111823803.html',
+      title: '花戛中学：给他们坚持读下去的力量'
+    },
+    {
+      pic: 6,
+      url: 'http://www.owecn.com/Life/20121115/2012111523815.html',
+      title: '福和希望小学：吃肉是孩子们的奢望'
+    },
+    {
+      pic: 7,
+      url: 'http://www.owecn.com/Life/20131101/2013110125134.html',
+      title: '希望她做一个对社会有用的人'
+    },
+    {
+      pic: 8,
+      url: 'http://www.owecn.com/Life/20111108/201111089071.html',
+      title: '我和媒体记者们不得不说的故事'
+    },
+    {
+      pic: 9,
+      url: 'http://www.owecn.com/Life/20130713/2013071323788.html',
+      title: '中村小学：人间处处有爱心'
+    }
+  ]
+}
